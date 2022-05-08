ffe402ed0066ffe30e937213499b10fcd7d0f50b
BalsRadu <bals.radu@gmail.com>
2022-05-08 21:32:01 +0300

    CM-1:The created acount its stored in the database

--- src/main/java/com/marketplace/fsbz_marketplace/UserRegister.java
+++ src/main/java/com/marketplace/fsbz_marketplace/UserRegister.java
@@ -5,15 +5,36 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
+import org.controlsfx.control.action.Action;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Statement;
 
 public class UserRegister {
     @FXML
     private Button registrationButton;
+    @FXML
+    private Label confirmPasswordLabel;
+    @FXML
+    private Label registrationMessageLabel;
+    @FXML
+    private TextField firstnameTextField;
+    @FXML
+    private TextField lastnameTextField;
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField setPasswordField;
+    @FXML
+    private PasswordField confirmPasswordField;
     @FXML
     private Button cancelButton;
 
@@ -28,5 +49,42 @@ public class UserRegister {
         stage.show();
     }
 
+    public void registerButtonOnAction(ActionEvent event){
+        if(setPasswordField.getText().equals(confirmPasswordField.getText())){
+            registerUser();
+        }else{
+            confirmPasswordLabel.setText("Password does not match");
+        }
+    }
+
+    public void registerUser(){
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+
+        String firstname = firstnameTextField.getText();
+        String lastname = lastnameTextField.getText();
+        String email = emailTextField.getText();
+        String username = usernameTextField.getText();
+        String password = setPasswordField.getText();
+
+        String insertFields = "INSERT INTO user_account(lastname,firstname,email,username,password) VALUES ('";
+        String insertValues = firstname+"','" +lastname+"','" +email+"','" +username+"','" +password+"')";
+        String insertToRegister = insertFields + insertValues;
+
+        try{
+
+            Statement statement = connectDB.createStatement();
+            statement.executeUpdate(insertToRegister);
+
+            registrationMessageLabel.setText("User has been registered succesfully!");
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+
+
+    }
 
 }
